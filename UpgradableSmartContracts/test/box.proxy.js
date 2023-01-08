7fb6c13ed3f81fc436db66680eda27643b5365f6
nachiket <nachiketdharmik@gmail.com>
2023-01-08 15:16:25 +0530

    Upgradable Smartcontract

--- UpgradableSmartContracts/test/box.proxy.js
+++ UpgradableSmartContracts/test/box.proxy.js
@@ -14,6 +14,7 @@ describe('Box (proxy)', function () {
  
   // Test case
   it('retrieve returns a value previously initialized', async function () {
+    
     // Test if the returned value is the same one
     // Note that we need to use strings to compare the 256 bit integers
     expect((await box.retrieve()).toString()).to.equal('42');
