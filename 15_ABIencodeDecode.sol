88555a26bfa0d2d472895daec0557e131817a509
nachiket <nachiketdharmik@gmail.com>
2023-01-12 10:47:24 +0530

    Solidity Contracts

---
+++ 15_ABIencodeDecode.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.4;
+
+contract ABIEncode{
+    uint x = 10;
+    address addr = 0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71;
+    string name = "0xAA";
+    uint[2] array = [5, 6]; 
+
+    function encode() public view returns(bytes memory result) {
+        result = abi.encode(x, addr, name, array);
+    }
+
+    function encodePacked() public view returns(bytes memory result) {
+        result = abi.encodePacked(x, addr, name, array);
+    }
+//to call a contract function refer DelegareCall contract
+    function encodeWithSignature() public view returns(bytes memory result) {
+        result = abi.encodeWithSignature("foo(uint256,address,string,uint256[2])", x, addr, name, array);
+    }
+//to call a contract function refer DelegareCall contract
+    function encodeWithSelector() public view returns(bytes memory result) {
+        result = abi.encodeWithSelector(bytes4(keccak256("foo(uint256,address,string,uint256[2])")), x, addr, name, array);
+    }
+    function decode(bytes memory data) public pure returns(uint dx, address daddr, string memory dname, uint[2] memory darray) {
+        (dx, daddr, dname, darray) = abi.decode(data, (uint, address, string, uint[2]));
+    }
+}
